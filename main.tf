ff132b6e6d8f5487e2210d6e9b5d211f03f97c5e
tecnicocefet <tecnicocefet@gmail.com>
2024-05-08 05:35:59 -0300

    fix

--- main.tf
+++ main.tf
@@ -45,9 +45,9 @@ module "eks" {
 
   eks_managed_node_groups = {
     default = {
-      min_size       = 4
-      max_size       = 4
-      desired_size   = 4
+      min_size       = 5
+      max_size       = 5
+      desired_size   = 5
       instance_types = var.aws_eks_managed_node_groups_instance_types
       tags           = var.aws_project_tags
     }
